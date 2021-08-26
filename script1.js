66bd8dddb410396ee9aec54bb1a40fb9d6fa9366
@bryancapulong <bryancarlocapulong@gmail.com>
2021-08-26 09:54:33 +0800

    Added password validation functions to the Membership form
    Added functions to validate if the password input contains at least 1 capital letter, and special ccharacter and if it is at least 8 characters long. Also editted the age field pattern to allow 2 digits or more as it only allowed 3 digits before

--- script1.js
+++ script1.js
@@ -25,12 +25,27 @@ function create1(){
 
     if(Password1 === "" || Password2 === ""|| UserName1 === "" || Age === "" || FirstName ==="" || LastName=== "" ){
         alert("Please Check the Fill up shit there seems a problem");
+        return false;
     }
     else if( Password1 !== Password2){
-        alert("Password Does not Match Retry Again")
+        alert("Password Does not Match Retry Again");
+        return false;
+    }
+    else if (Password1.length < 8) {
+        alert("Password must contain at least 8 characters");
+        return false;
+    }
+    else if (isThereUpperCase(Password1) === false) {
+        alert("Password must include at least one(1) Capital letter.");
+        return false;
+    }
+    else if (containsSpecialChar(Password1) === false) {
+        alert("Password must include at least one(1) Special character.");
+        return false;
     }
     else {
         alert( UserName1 + "" + " Account is registered Successfully");  
+        
     }
     
 }
@@ -41,4 +56,26 @@ function create1(){
 function UserJS(){
     let UserName = document.getElementById("UserName").value ="jerjer ";
 
+}
+
+
+function isUpperCase(char) {
+    if (char >= 'A' && char <= 'Z') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isThereUpperCase(str) {
+    let thereIsUpperCase = false;
+    for (let i = 0; i < str.length; i++) {
+        if (isUpperCase(str.charAt(i)) === true) thereIsUpperCase = true;
+    }
+    return thereIsUpperCase;
+}
+
+function containsSpecialChar(str) {
+    let specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
 }
